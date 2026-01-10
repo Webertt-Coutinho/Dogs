7224db76a237441f88cf09e5acc2059078f2e3d6
Webertt Coutinho <webertt.coutinho@gmail.com>
2026-01-10 09:07:26 -0300

    Photo Delete

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,8 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from './PhotoContent.module.css'
 import PhotoComments from './PhotoComments';
+import { use, useContext } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
 
 const PhotoContent = ({data}) => {
+  const user = useContext(UserContext);
   const {photo, comments} = data;
 
   return (
@@ -14,7 +18,9 @@ const PhotoContent = ({data}) => {
     <div className={styles.details}>
       <div>
         <p className={styles.author}>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          {user.data && user.data.username === photo.author? (
+            <PhotoDelete id={photo.id}/>
+          ) : <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>}
           <span className={styles.vizualizacoes}>{photo.acessos}</span>
         </p>
         <h1 className='title'>
